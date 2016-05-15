b0be95b7b443fb6cff5ead7da4eb05d19b81dd09
Stanislav Bashkyrtsev <stanislav.bashkirtsev@gmail.com>
2016-05-16 00:54:56 +0300

    Set dependencies and basic REST configuration

---
+++ src/main/java/com/oecontrib/microservices/v1/HomeEndpoint.java
@@ -0,0 +1,15 @@
+package com.oecontrib.microservices.v1;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeEndpoint {
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Microservices that expose OpenEye Toolkits functionality as REST services.";
+    }
+
+}
