234a17bd227d12ffdbfa6f5480641967c5c305d7
Stanislav Bashkyrtsev <stanislav.bashkirtsev@gmail.com>
2016-05-21 14:16:33 +0300

    Factor out molecules into test resources

--- src/test/java/com/oecontrib/microservices/v2/ConvertingEndpointTest.java
+++ src/test/java/com/oecontrib/microservices/v2/ConvertingEndpointTest.java
@@ -9,6 +9,12 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import static com.jayway.restassured.module.mockmvc.RestAssuredMockMvc.given;
 import static com.jayway.restassured.module.mockmvc.RestAssuredMockMvc.mockMvc;
 import static io.qala.datagen.RandomShortApi.alphanumeric;
@@ -25,14 +31,13 @@ public class ConvertingEndpointTest {
     }
 
     @Test public void convertSmilesToSmilesReturnsSameString() {
-        String smiles = "c1c(c(cc(c1F)F)F)C[C@H](CC(=O)N2CCn3c(nnc3C(F)(F)F)C2)N";
-        MockMvcResponse response = convert(smiles, "smiles");
-        assertEquals(smiles + "\n", response.asString());
+        MockMvcResponse response = convert(molecule1("smiles"), "smiles");
+        assertEquals(molecule1("smiles"), response.asString());
     }
 
     @Test public void convertSmilesToSmilesReturnsDifferentRepresentationOfSmiles() {
-        MockMvcResponse response = convert("Fc1cc(c(F)cc1F)C[C@@H](N)CC(=O)N3Cc2nnc(n2CC3)C(F)(F)F");
-        assertEquals("c1c(c(cc(c1F)F)F)C[C@H](CC(=O)N2CCn3c(nnc3C(F)(F)F)C2)N\n", response.asString());
+        MockMvcResponse response = convert(molecule1("smiles-not-normalized"));
+        assertEquals(molecule1("smiles"), response.asString());
     }
 
     @Test public void convertMoleculeToInvalidFormatReturns400BadRequest() {
@@ -43,22 +48,7 @@ public class ConvertingEndpointTest {
     }
     @Test public void convertMoleculeToMol2() {
         MockMvcResponse response = convert("c1ccc1", "mol2");
-        assertEquals("@<TRIPOS>MOLECULE\n" +
-                "*****\n" +
-                "    4     4     0     0     0\n" +
-                "SMALL\n" +
-                "NO_CHARGES\n" +
-                "\n" +
-                "@<TRIPOS>ATOM\n" +
-                "      1 C1          0.0000    0.0000    0.0000 C.2       1 <0>         0.0000\n" +
-                "      2 C2          0.0000    0.0000    0.0000 C.2       1 <0>         0.0000\n" +
-                "      3 C3          0.0000    0.0000    0.0000 C.2       1 <0>         0.0000\n" +
-                "      4 C4          0.0000    0.0000    0.0000 C.2       1 <0>         0.0000\n" +
-                "@<TRIPOS>BOND\n" +
-                "     1    1    4 2\n" +
-                "     2    1    2 1\n" +
-                "     3    2    3 2\n" +
-                "     4    3    4 1\n", response.asString());
+        assertEquals(molecule1("mol2"), response.asString());
     }
 
     private MockMvcResponse convert(String smiles) {
@@ -70,5 +60,14 @@ public class ConvertingEndpointTest {
                 param("val", smiles).
                 header("Accepts", outputFormat).
                 get("/v2/structure");
+    }
+
+    private String molecule1(String format) {
+        try {
+            Path path = Paths.get(getClass().getResource("/testmolecules/m1." + format).toURI());
+            return new String(Files.readAllBytes(path));
+        } catch (IOException | URISyntaxException e) {
+            throw new IllegalArgumentException("Bug in test - wrong molecule format was requested: " + format);
+        }
     }
 }
